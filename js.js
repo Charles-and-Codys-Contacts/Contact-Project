3d615ee1465139bcf2b2ddd6f64bacf4286bfedb
Cody Hodges <hodges.cody00@gmail.com>
2022-03-11 13:59:21 -0600

    Push

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 $.get('data/contacts.txt').done(function (data) {
 
 
+
     let people = [];
     people = data.split("\n");
 
@@ -36,44 +37,37 @@ $.get('data/contacts.txt').done(function (data) {
 
         $("#description").append(`<p id="name"></i>${split[0]}</p><br><p id="number">${split[1]}</p>`);
 
-
+        $.get('https://dog.ceo/api/breeds/image/random').done(function (dogs){
+            let dog = dogs.message;
+            console.log(dogs)
+            $("#person").prepend(`<img src="${dog}">`)
         if (count === 0) {
             $("#box").addClass("toggle1")
-            $("#person").prepend(`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSD-l889V8_Nv64SYZECELEBUzvWgmgxdlAow&usqp=CAU">`)
         } else if (count === 1) {
             $("#box").addClass("toggle2")
-            $("#person").prepend(`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe-nDXn1Xg8qOP0odcLuOkPZ7kpLzeGI-3FQ&usqp=CAU">`)
         } else if (count === 2) {
             $("#box").addClass("toggle3")
-            $("#person").prepend(`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGqeiMvcMA8ATx6McIgv0QgGq9njL6_9Q9Ww&usqp=CAU">`)
         } else if (count === 3) {
             $("#box").addClass("toggle4")
-            $("#person").prepend(`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLHvzyqlpe7Aw_qH5ZR5fvjErwjzNuqIlc6A&usqp=CAU">`)
         } else if (count === 4) {
             $("#box").addClass("toggle5")
-            $("#person").prepend(`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT16eO5W8VPjVFrkvG8n_2FQKjByMcbLtBF4A&usqp=CAU">`)
         } else if (count === 5) {
             $("#box").addClass("toggle6")
-            $("#person").prepend(`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6zes53m4a_2VLTcmTn_bHk8NO5SkuWfcQbg&usqp=CAU">`)
         } else if (count === 6) {
             $("#box").addClass("toggle7")
-            $("#person").prepend(`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRsC5vv2K4miQP22QyiC3x6vjLdkmzUITLKQ&usqp=CAU">`)
         } else if (count === 7) {
             $("#box").addClass("toggle8")
-            $("#person").prepend(`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCT6ifq3Wo5EACfxc3xIeVVdogS_OJsjHOdw&usqp=CAU">`)
         } else if (count === 8) {
             $("#box").addClass("toggle9")
-            $("#person").prepend(`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmY2DS_XaT4MZa-O3OdDFn78MV4PX_kcU_ag&usqp=CAU">`)
         } else if (count === 9) {
             $("#box").addClass("toggle10")
-            $("#person").prepend(`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSD8197V5L6HbodA7IlH-Rimsb0NRhLhLBEvw&usqp=CAU">`)
         } else if (count === 10) {
             $("#box").addClass("toggle11")
-            $("#person").prepend(`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo8eAAlNI5l9m4zATOTvt6h8kWbHzZH5fM6g&usqp=CAU">`)
+
         }
         console.log(count)
         count = 0;
-
+        });
     })
 
 
