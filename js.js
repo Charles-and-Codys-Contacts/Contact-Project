10404082cd89572b9804435933e3d08cd822178a
Cody Hodges <hodges.cody00@gmail.com>
2022-03-11 11:35:20 -0600

    Cody

--- js.js
+++ js.js
@@ -6,12 +6,32 @@ $.get('data/contacts.txt').done(function (data) {
 
 
     // ON CLICK APPENDS RANDOM PERSON
-    $("#box").on("click", function (){
-        let random = people[Math.floor(Math.random() * (people.length - 1))];
+    $("#showLine").on("click", function (){
+        let count = Math.floor(Math.random() * 2);
+
+        if (count %2 == 0){
+            $("#box").html("")
+            $("#box").append(`<div id="person">
+            <p id="description"></p>
+        </div>`)
+            $("#box").toggleClass("toggle1")
+            let random = people[Math.floor(Math.random() * (people.length - 1))];
+            $("#person").prepend(`<img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png">`)
+            $("#description").append(random + `<br>`);
+
+        }else {
+            $("#box").html("")
+            $("#box").append(`<div id="person">
+            <p id="description"></p>
+        </div>`)
+            $("#box").toggleClass("toggle2")
+            let random = people[Math.floor(Math.random() * (people.length - 1))];
+            $("#person").prepend(`<img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png">`)
+            $("#description").append(random + `<br>`);
+
+        }
 
 
-
-        $("#description").append(random + `<br>`);
     })
 
 
